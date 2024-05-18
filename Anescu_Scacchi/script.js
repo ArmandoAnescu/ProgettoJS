3f2a15fccd5ae95a752974efe6c840bbe72ecd5e
Armando <luis.anescu@iisviolamarchesini.edu.it>
2024-05-18 18:33:45 +0200

    Correto invertimento dati nella tabella

--- Anescu_Scacchi/script.js
+++ Anescu_Scacchi/script.js
@@ -1,108 +1,100 @@
 const pezzi = document.getElementsByClassName("Pezzo");
 const Img = document.querySelectorAll("img");
 const caselle = document.getElementsByClassName("spazio");
-let turnoBianco=true;
-whiteCounter=0;
-blackCounter=0;
-let punteggio=document.body.querySelectorAll("#Counter");
-punteggio[0].textContent=whiteCounter;
-punteggio[1].textContent=whiteCounter;
+let turnoBianco = true;
+whiteCounter = 0;
+blackCounter = 0;
+let punteggio = document.body.querySelectorAll("#Counter");
 Spostamento();
 SpostamentoPezzi();
-function Spostamento()
+function AggiornaPunteggio()
 {
-    for(let i=0;i<caselle.length; i++)
-    {
-        caselle[i].addEventListener("dragover",allowDrop);
-        caselle[i].addEventListener("drop",drop);
-        let riga=8-Math.floor(i/8);
-        let colonna=String.fromCharCode(97+(i%8));
-        let casella=caselle[i];
-        casella.id=colonna+riga;
-    }
-}
-function SpostamentoPezzi()
-{
-    for(let i=0;i<pezzi.length;i++)
-    {
-        pezzi[i].addEventListener("dragstart",drag);
-        pezzi[i].setAttribute("draggable",true);
-        pezzi[i].id=pezzi[i].className.split(" ")[1]+" "+pezzi[i].parentElement.id;
-    }
-    for(let i=0;i<Img.length;i++)
-    {
-        Img[i].setAttribute("draggable",false);
-    }
-    
-}
-function ControlloTurno(ev)
-{
-    let spostamento=true;
-    if(turnoBianco){
-        if(ev.classList.contains("Bianco")){
-            spostamento=true;
-            turnoBianco=false;
+    for (let i = 0; i < punteggio.length; i++) {
+        if (i == 0) {
+            punteggio[i].textContent=whiteCounter;
         }else
         {
-            spostamento=false;
+            punteggio[i].textContent=blackCounter;
         }
-    }else
-    {
-        if(ev.classList.contains("Nero")){
-            spostamento=true;
-            turnoBianco=true;
-        }else
-        {
-            spostamento=false;
+    }
+}
+function Spostamento() {
+    for (let i = 0; i < caselle.length; i++) {
+        caselle[i].addEventListener("dragover", allowDrop);
+        caselle[i].addEventListener("drop", drop);
+        let riga = 8 - Math.floor(i / 8);
+        let colonna = String.fromCharCode(97 + (i % 8));
+        let casella = caselle[i];
+        casella.id = colonna + riga;
+    }
+}
+function SpostamentoPezzi() {
+    for (let i = 0; i < pezzi.length; i++) {
+        pezzi[i].addEventListener("dragstart", drag);
+        pezzi[i].setAttribute("draggable", true);
+        pezzi[i].id = pezzi[i].className.split(" ")[1] + " " + pezzi[i].parentElement.id;
+    }
+    for (let i = 0; i < Img.length; i++) {
+        Img[i].setAttribute("draggable", false);
+    }
+}
+function ControlloTurno(ev) {
+    let spostamento = true;
+    if (turnoBianco) {
+        if (ev.classList.contains("Bianco")) {
+            spostamento = true;
+            turnoBianco = false;
+        } else {
+            spostamento = false;
+        }
+    } else {
+        if (ev.classList.contains("Nero")) {
+            spostamento = true;
+            turnoBianco = true;
+        } else {
+            spostamento = false;
         }
     }
     return spostamento;
 }
-function allowDrop(ev)
-{
+function allowDrop(ev) {
     ev.preventDefault();
 }
-function drag(ev)
-{
-  const pezzo=ev.target;
-  if(ControlloTurno(pezzo))
-    {
-        ev.dataTransfer.setData("text",pezzo.id); 
+function drag(ev) {
+    const pezzo = ev.target;
+    if (ControlloTurno(pezzo)) {
+        ev.dataTransfer.setData("text", pezzo.id);
     }
 }
-function ControlloCattura(ev,pezzo)
-{
-    let pezzoDaMangiare=ev.querySelector(".Pezzo");
-    if(pezzoDaMangiare)
-    {
-        if(pezzoDaMangiare.classList.contains("Bianco")&& pezzo.classList.contains("Nero")){
-            whiteCounter++;
-            ev.removeChild(pezzoDaMangiare);
-            return true;
-        }else if(pezzoDaMangiare.classList.contains("Nero") && pezzo.classList.contains("Bianco"))
-        {
+function ControlloCattura(ev, pezzo) {
+    let pezzoDaMangiare = ev.querySelector(".Pezzo");
+    if (pezzoDaMangiare) {
+        if (pezzoDaMangiare.classList.contains("Bianco") && pezzo.classList.contains("Nero")) {
             blackCounter++;
             ev.removeChild(pezzoDaMangiare);
             return true;
-        }else
-        {
+        } else if (pezzoDaMangiare.classList.contains("Nero") && pezzo.classList.contains("Bianco")) {
+            whiteCounter++;
+            ev.removeChild(pezzoDaMangiare);
+            return true;
+        } else {
             return false;
         }
-    }else
-    {
+    } else {
         return true;
     }
 }
-function drop(ev)
-{
+function drop(ev) {
     ev.preventDefault();
-    let data=ev.dataTransfer.getData("text");
-    const pezzo=document.getElementById(data)
-    const destinazione=ev.currentTarget;
-    let destinazioneId=destinazione.id;
-    if(ControlloCattura(destinazione,pezzo)){
+    let data = ev.dataTransfer.getData("text");
+    const pezzo = document.getElementById(data)
+    const destinazione = ev.currentTarget;
+    let destinazioneId = destinazione.id;
+    if (ControlloCattura(destinazione, pezzo)) {
         destinazione.appendChild(pezzo);
     }
+    AggiornaPunteggio();
+    
 }
 /*
 gli otto pedoni si muovono solo in avanti e mangiano in obliquo;
